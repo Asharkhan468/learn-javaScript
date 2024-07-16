a01b4be52e1a98410dc5dfb9c04a60b6902f2596
Ashar Khan <asharullah.khan2468@gmail.com>
2024-07-16 18:21:33 +0500

    project

--- home.js
+++ home.js
@@ -3,6 +3,56 @@ let data = JSON.parse(localStorage.getItem('data'));
 console.log(data);
 
 const div = document.querySelector("#div");
+const para = document.querySelector("#para");
+const totalExpense = document.querySelector("#total-expense");
 
-div.innerHTML += `<h2>Your Balance<br><span> ${data} PKR </span></h2>`;
+const list = document.querySelector(".transaction-list");
+const expense = document.querySelector("#expense");
+const description = document.querySelector("#description");
+
+let balance = data
+
+
+div.innerHTML += '$'+`${data}`;
+
+para.innerHTML ="$"+`${data}`;
+
+const addBtn = document.querySelector("#Add-btn");
+
+addBtn.addEventListener('click' , ()=>{
+    list.innerHTML += `
+     <li class="transaction">
+  <span class="expense-name">${description.value}</span>
+                <span class="expense-amount text-danger"> $${expense.value} </span>
+                <span><i class="fa-solid fa-xmark"></i></span>
+</li>
+    `;
+
+    let expenseValue = parseInt(expense.value);
+
+   balance -= expenseValue
+
+   console.log(balance);
+
+   localStorage.setItem("currentBalance" , balance);
+
+   let currentBalance = JSON.parse(localStorage.getItem("currentBalance"));
+
+   document.addEventListener("DOMContentLoaded", () => {
+     
+   });
+
+
+    expense.value=""
+    description.value=""
+
+
+
+})
+
+
+
+
+
+
 
